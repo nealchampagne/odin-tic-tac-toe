508b8cd8994de0f6e6a77c96a965c870475dc45d
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-06-28 11:04:27 -0700

    Implement board object, write some game functions
    Implement board object with array that holds game board state as well as
    functions in game object to write to the board and check for wins.

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const game = () => {};
+const game = () => {
+  const writeBoardX = (x,y) => {
+    gameBoard.board[x][y] = 'X';
+    if (checkWin() === 'winX') {
+      console.log(`Player X wins!`);
+    } else if (checkWin() === 'tie') {
+      console.log(`It's a tie!`);
+    };
+  };
 
-const gameBoard = () => {};
+  const writeBoardO = (x,y) => {
+    gameBoard.board[x][y] = 'O';
+    if (checkWin() === 'winO') {
+      console.log(`Player O wins!`);
+    } else if (checkWin() === 'tie') {
+      console.log(`It's a tie!`);
+    };
+  };
+
+  const checkWin = () => {
+    for(let i = 0; i < 3; i++) {
+      if(gameBoard.board[i][0] + gameBoard.board[i][1] + gameBoard.board[i][2] === 'XXX' ||
+        gameBoard.board[0][i] + gameBoard.board[1][i] + gameBoard.board[2][i] === 'XXX') {
+        return 'winX'
+      } else if (gameBoard.board[i][0] + gameBoard.board[i][1] + gameBoard.board[i][2] === 'OOO' ||
+        gameBoard.board[0][i] + gameBoard.board[1][i] + gameBoard.board[2][i] === 'OOO') {
+        return 'winO';
+      };
+    };
+    if(gameBoard.board[0][0] + gameBoard.board[1][1] + gameBoard.board[2][2] === 'XXX' ||
+      gameBoard.board[0[2]] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'XXX') {
+      return 'winX';
+    } else if(gameBoard.board[0][0] + gameBoard.board[1][1] + gameBoard.board[2][2] === 'OOO' ||
+      gameBoard.board[0][2] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'OOO') {
+      return 'winO';
+    };
+    if (gameBoard.board.length === 9) {
+      return 'tie';
+    } else {return false};
+  };
+
+  return {writeBoardX, writeBoardO, checkWin}
+};
+
+const gameBoard = (() => {
+  const board = [[,,],[,,],[,,]];
+  return {board};
+})();
 
 const player = () => {};
+
+const display = () => {};

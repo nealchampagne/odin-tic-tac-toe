ce0e0371c39531d7f89f77aa5048272e1ffd6d86
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-02 15:05:06 -0700

    Finish adding display functionality

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-const game = (() => {
-  // const getValue = () => ;
+function gameBoard() {
+  let board = [];
+  const rows = 3;
+  const cols = 3;
 
-  const printBoard = () => {
-    const gridState = gameBoard.board.map((row) => 
-      row.map((cell) => cell))
-    console.log(gridState);
-  };
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push('');
+    }
+  }
+
+  const getBoard = () => board;
+
+  const writeToBoard = (x,y,mark) => {
+    if(getBoard()[x][y] === '') {
+      getBoard()[x][y] = mark;
+      return true;
+    } else {
+      alert('Please choose an empty square!');
+      return false;
+    }
+  }
+
+  return {getBoard, writeToBoard};
+};
+
+const game = (() => {
+  const board = gameBoard();
 
   const checkWin = () => {
-    for(let i = 0; i < 3; i++) {
-      if(gameBoard.board[i][0] + gameBoard.board[i][1] + gameBoard.board[i][2] === 'XXX' ||
-        gameBoard.board[0][i] + gameBoard.board[1][i] + gameBoard.board[2][i] === 'XXX') {
-        return 'winX'
-      } else if (gameBoard.board[i][0] + gameBoard.board[i][1] + gameBoard.board[i][2] === 'OOO' ||
-        gameBoard.board[0][i] + gameBoard.board[1][i] + gameBoard.board[2][i] === 'OOO') {
-        return 'winO';
+    const content = board.getBoard();
+      for(let i = 0; i < 3; i++) {
+      if(content[i][0] + content[i][1] + content[i][2] === 'XXX' ||
+        content[0][i] + content[1][i] + content[2][i] === 'XXX') {
+        return `Player One wins!`
+      } else if (content[i][0] + content[i][1] + content[i][2] === 'OOO' ||
+        content[0][i] + content[1][i] + content[2][i] === 'OOO') {
+        return `Player Two wins!`;
       };
     };
-    if(gameBoard.board[0][0] + gameBoard.board[1][1] + gameBoard.board[2][2] === 'XXX' ||
-      gameBoard.board[0][2] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'XXX') {
-      return 'winX';
-    } else if(gameBoard.board[0][0] + gameBoard.board[1][1] + gameBoard.board[2][2] === 'OOO' ||
-      gameBoard.board[0][2] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'OOO') {
-      return 'winO';
+    if(content[0][0] + content[1][1] + content[2][2] === 'XXX' ||
+      content[0][2] + content[1][1] + content[2][0] === 'XXX') {
+      return `Player One wins!`;
+    } else if(content[0][0] + content[1][1] + content[2][2] === 'OOO' ||
+      content[0][2] + content[1][1] + content[2][0] === 'OOO') {
+      return `Player Two wins!`;
     };
-    if (!gameBoard.board[0].includes('-') &&
-      !gameBoard.board[1].includes('-') &&
-      !gameBoard.board[2].includes('-'))
-      {return 'draw'};
+    if (!content[0].includes('') &&
+      !content[1].includes('') &&
+      !content[2].includes(''))
+      {return `It's a draw!`};
   };
 
   const players = [
@@ -49,26 +71,41 @@ const game = (() => {
 
   const getCurrentPlayer = () => currentPlayer;
 
-  const writeBoard = (x,y,mark) => {
-    gameBoard.board[x][y] = mark;
-    if (checkWin() === 'winX') {
-      console.log(`Player One wins!`);
-    } else if (checkWin() === 'winO') {
-      console.log(`Player Two wins!`);
-    } else if (checkWin() === 'draw') {
-      console.log(`It's a draw!`);
-    };
-    printBoard();
-    alternateTurn();
-  };
-
-  return {writeBoard, printBoard, currentPlayer, getCurrentPlayer, alternateTurn};
+  const takeTurn = (x,y) => board.writeToBoard(x,y,getCurrentPlayer().mark);
+  
+  return {checkWin, alternateTurn, takeTurn, getCurrentPlayer, getBoard: board.getBoard};
 })();
 
-const gameBoard = (() => {
-  const board = [['-','-','-'],['-','-','-'],['-','-','-']];
-  console.log(board);
-  return {board};
-})();
+const display = () => {
+  const statusDiv = document.querySelector('.status');
+  const boardDiv = document.querySelector('.board');
+  const resetButton = document.querySelector('.reset');
 
-const display = () => {};
+  for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+      const square = document.createElement('button');
+      square.classList.add('square');
+      square.addEventListener('click', () => {
+        if(game.checkWin()) {
+          return;
+        }
+        if(game.takeTurn(i,j)) {
+          square.textContent = game.getCurrentPlayer().mark
+          if(game.checkWin()) {
+            statusDiv.textContent = game.checkWin();
+          } else {
+          game.alternateTurn();
+          statusDiv.textContent = `${game.getCurrentPlayer().name}'s turn`
+          }
+        }   
+      });
+      boardDiv.appendChild(square);
+    }
+  }
+
+  resetButton.addEventListener('click', () => {
+    location.reload();
+  });
+};
+
+display();

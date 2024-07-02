0dba3db9b5ad03c209282c58e6f246c57c442623
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-02 09:02:49 -0700

    Implement players and alternating turns

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const game = (() => {
-  const writeBoard = (x,y,mark) => {
-    gameBoard.board[x][y] = mark;
-    if (checkWin() === 'winX') {
-      console.log(`Player X wins!`);
-    } else if (checkWin() === 'winO') {
-      console.log(`Player O wins!`);
-    } else if (checkWin() === 'tie') {
-      console.log(`It's a tie!`);
-    };
-    console.log(gameBoard.board);
-    console.log(gameBoard.board.length);
+  // const getValue = () => ;
+
+  const printBoard = () => {
+    const gridState = gameBoard.board.map((row) => 
+      row.map((cell) => cell))
+    console.log(gridState);
   };
 
   const checkWin = () => {
@@ -23,27 +18,57 @@ const game = (() => {
       };
     };
     if(gameBoard.board[0][0] + gameBoard.board[1][1] + gameBoard.board[2][2] === 'XXX' ||
-      gameBoard.board[0[2]] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'XXX') {
+      gameBoard.board[0][2] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'XXX') {
       return 'winX';
     } else if(gameBoard.board[0][0] + gameBoard.board[1][1] + gameBoard.board[2][2] === 'OOO' ||
       gameBoard.board[0][2] + gameBoard.board[1][1] + gameBoard.board[2][0] === 'OOO') {
       return 'winO';
     };
-    if (!gameBoard.board[0].includes('') &&
-      !gameBoard.board[1].includes('') &&
-      !gameBoard.board[2].includes(''))
-      {return 'tie'};
+    if (!gameBoard.board[0].includes('-') &&
+      !gameBoard.board[1].includes('-') &&
+      !gameBoard.board[2].includes('-'))
+      {return 'draw'};
   };
 
-  return {writeBoard, checkWin}
+  const players = [
+    {
+      name: 'Player One',
+      mark: 'X'
+    },
+    {
+      name: 'Player Two',
+      mark: 'O'
+    }
+  ];
+
+  let currentPlayer = players[0];
+
+  const alternateTurn = () => {
+    currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  const writeBoard = (x,y,mark) => {
+    gameBoard.board[x][y] = mark;
+    if (checkWin() === 'winX') {
+      console.log(`Player One wins!`);
+    } else if (checkWin() === 'winO') {
+      console.log(`Player Two wins!`);
+    } else if (checkWin() === 'draw') {
+      console.log(`It's a draw!`);
+    };
+    printBoard();
+    alternateTurn();
+  };
+
+  return {writeBoard, printBoard, currentPlayer, getCurrentPlayer, alternateTurn};
 })();
 
 const gameBoard = (() => {
-  const board = [['','',''],['','',''],['','','']];
+  const board = [['-','-','-'],['-','-','-'],['-','-','-']];
   console.log(board);
   return {board};
 })();
-
-const player = () => {};
 
 const display = () => {};
